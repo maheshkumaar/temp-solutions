3b64a1ad0b18b2eb8fcdb5dd8a7f340227075b0f
Maheshkumaar <maheshkumaar2106@gmail.com>
2017-06-06 22:08:50 +0530

    Once again

--- server/connection/mongoose.js
+++ server/connection/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('localhost:27017/TempSolutions');
+//mongoose.connect('localhost:27017/TempSolutions');
 
-// mongoose.connect('mongodb://maheshkumaar:cryptic123@ds163681.mlab.com:63681/temp-solutions');
+ mongoose.connect('mongodb://maheshkumaar:cryptic123@ds163681.mlab.com:63681/temp-solutions');
 
 module.exports = {mongoose};
